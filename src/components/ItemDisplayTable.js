e997c57dd364aaa788c943a93a7f637127be778e
Grayson Howard <mhoward@mtu.edu>
2022-05-13 15:25:09 -0400

    Layout for displaying a potlucks items
    DISPLAY ONLY so far

--- src/components/ItemDisplayTable.js
+++ src/components/ItemDisplayTable.js
@@ -1,4 +1,56 @@
-export default function ItemDisplayTable(){
+import { useState } from "react"
 
-    return(<></>)
+
+export default function ItemDisplayTable(id){
+
+    cosnt [items, setItems] = useState([]);
+    const userInfo = JSON.parse(sessionStorage.getItem(""))
+
+    //Get data from backend
+    async function getItemsForPotlukk(){
+        const response = await fetch(`localhost:8080/potlukks/${id}/items`);
+        const body = await response.json();
+        setItems(body)
+    }
+
+    useEffect(()=>{
+        getItemsForPotlukk();
+    },[]);
+
+    
+    const itemRows = items.map(item =>
+        <tr key={item.id}>
+            <td>{item.name}</td>
+            <td><nameOrButton/></td>
+            <td>{item.status}</td>
+            <td><deleteItem/></td>
+        </tr>)
+
+    function nameOrButton(supplier){
+        let display;
+        if(supplier){
+            display = supplier;
+        }else{
+            display = <button>Sign Up!</button>
+        }
+        return display
+    }
+    function deleteItem(){
+
+    }
+
+    return(<>
+        <table>
+            <thead>
+                <tr>
+                    <th>Item</th>
+                    <th>Supplier</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                {itemRows}
+            </tbody>
+        </table>
+    </>)
 }
