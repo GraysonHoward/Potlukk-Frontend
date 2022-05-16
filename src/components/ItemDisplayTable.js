5dce38a42759f96930f622a8bef1df82e8d67cc2
Grayson Howard <mhoward@mtu.edu>
2022-05-16 16:29:46 -0400

    Radio for status

--- src/components/ItemDisplayTable.js
+++ src/components/ItemDisplayTable.js
@@ -1,8 +1,4 @@
-export default function ItemDisplayTable(){
-
-<<<<<<< Updated upstream
-    return(<></>)
-=======
+import { useState, useEffect } from "react";
 
 export default function ItemDisplayTable(id){
 
@@ -10,7 +6,7 @@ export default function ItemDisplayTable(id){
     const userInfo = JSON.parse(sessionStorage.getItem("user"));
     const [potlukk, setPotlukk] = useState({});
     const [showButton, setShowButton] = useState(true)
-    const [itemRow, setItemRow] = useState({id:0, name:'', provider:'', status:''})
+    const [showSignUp, setShowSignUp] = useState(true)
 
     //Get data from backend
     async function getItemsForPotlukk(){
@@ -33,37 +29,50 @@ export default function ItemDisplayTable(id){
     const [itemID, setItemID] = useState("");
     const [item, setItem] = useState("");
     const [supplier, setSupplier] = useState("");
+    const [status, setStatus] = useState("");
 
     // Set state values
     function updateItemsetItem(event){
         setItem(event.target.value);
     }
-    function updateProvider(event){
+    function updateSupplier(event){
         setSupplier(event.target.value);
     }
 
     function addItemInput(){
         let component;
-        if(userInfo){
+        if(userInfo.uId === -1){
             component = <>
                 <label htmlFor="item">Item</label>
-                <input onChange={updateItemsetItem} name="item" value={item}/>
+                <input onChange={updateItemsetItem} name="item" value={item}/><br/>
                 setSupplier(userInfo.username)
+                <label htmlFor="WANTED">Wanted </label>
+                <input type="radio" onClick={() => setStatus("WANTED")}/><br/>
+                <label htmlFor="NEEDED">Needed </label>
+                <input type="radio" onClick={() => setStatus("NEEDED")}/><br/>
+                <label htmlFor="FULFILLED">Fulfilled </label>
+                <input type="radio" onClick={() => setStatus("FULFILLED")}/><br/>
                 <button onClick={addItem}>Add</button>
             </>
         }else{
             component = <>
                 <label htmlFor="item">Item</label>
-                <input onChange={updateItemsetItem} type="text" value={item}/>
-                <label htmlFor="Provider">Provider</label>
-                <input onChange={updateProvider} value={supplier}/>
+                <input onChange={updateItemsetItem} type="text" value={item}/><br/>
+                <label htmlFor="Supplier">Supplier</label>
+                <input onChange={updateSupplier} value={supplier}/><br/>
+                <label htmlFor="WANTED">Wanted </label>
+                <input type="radio" onClick={() => setStatus("WANTED")}/><br/>
+                <label htmlFor="NEEDED">Needed </label>
+                <input type="radio" onClick={() => setStatus("NEEDED")}/><br/>
+                <label htmlFor="FULFILLED">Fulfilled </label>
+                <input type="radio" onClick={() => setStatus("FULFILLED")}/><br/>
                 <button onClick={addItem}>Add</button>
             </>
         }
         return component;
     }
     async function addItem(){
-        const itemObj = {id:0, name:item, supplier:supplier, status:"FULFILLED", potlukkID:id.value}
+        const itemObj = {id:0, name:item, supplier:supplier, status:status, potlukkID:id.value}
 
         await fetch("http://localhost:8080/items",{
             body:JSON.stringify(itemObj),
@@ -75,10 +84,12 @@ export default function ItemDisplayTable(id){
         
         //dont do this
         getItemsForPotlukk()
+
+        setShowButton(false)
     }
 
     function SignUpInput(item_id){
-        if(userInfo){
+        if(userInfo.uId === -1){
             setItemID(item_id);
             setSupplier(userInfo.username);
             signUp();
@@ -86,8 +97,8 @@ export default function ItemDisplayTable(id){
         else{
             setItemID(item_id);
             <>
-                <label htmlFor="Provider">Provider</label>
-                <input onChange={updateProvider} name="provider" text="provider" placeholder="Name"/>
+                <input onChange={updateItemsetItem} type="text" value={item}/>
+                <label htmlFor="Supplier">Supplier</label>
                 <button onClick={signUp}>Sign Up!</button>
             </>
         }
@@ -102,7 +113,7 @@ export default function ItemDisplayTable(id){
                 "Content-Type":"application/json"
             }     
         });
-
+        getItemsForPotlukk()
     }
 
 
@@ -121,14 +132,14 @@ export default function ItemDisplayTable(id){
         if(args.supplier){
             display = <>{args.supplier}</>;
         }else{
-            display = <button onClick={() => SignUpInput(args.item_id)}>Sign Up!</button>
+            {(showSignUp) ? <button onClick={() => setShowSignUp(false)}>Sign Up!</button> : SignUpInput() }
         }
         return display
     }
     function DeleteItem(){ // Give option to delete item iff current user is potlukk owner
         let deleteButton;
-        if(userInfo){
-            if(potlukk.hostID===userInfo.id){
+        if(userInfo.uId === -1){
+            if(potlukk.hostID===userInfo.uId){
                 deleteButton = <button>Delete</button>
             }
             else{
@@ -155,5 +166,4 @@ export default function ItemDisplayTable(id){
         {(showButton) ? <button onClick={() => setShowButton(false)}>Add Item</button> : addItemInput() }
         
     </>)
->>>>>>> Stashed changes
 }
